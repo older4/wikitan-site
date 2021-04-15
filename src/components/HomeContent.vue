2100282684f890cc8a85bd4ba78a6ab8c4d33b9b
ryo <3955027+older4@users.noreply.github.com>
2021-04-16 01:51:59 +0900

    少しレイアウトを変更

--- src/components/HomeContent.vue
+++ src/components/HomeContent.vue
@@ -3,8 +3,8 @@
     <h1 class="text-md-h2 text-h4 black--text text-center">作者</h1>
     <a
       href="https://twitter.com/YasaiDev"
-      class="text-center text-h6 text-decoration-none black--text"
-      ><p><v-icon color="blue">mdi-twitter</v-icon>@YasaiDev</p></a
+      class="text-center text-body-1 text-decoration-none black--text"
+      ><p class="pt-4"><v-icon color="blue">mdi-twitter</v-icon>@YasaiDev</p></a
     >
   </div>
 </template>
