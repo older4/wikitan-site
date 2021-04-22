4f58d64d2daf59e2b563051727e9f1a4cce16a48
ryo <3955027+older4@users.noreply.github.com>
2021-04-22 14:21:22 +0900

    add itunes url

--- src/components/StartButton.vue
+++ src/components/StartButton.vue
@@ -40,6 +40,12 @@ export default {
         const launch_uri = `wikitan://topic?start=${encodeURIComponent(
           this.startword
         )}&end=${encodeURIComponent(this.endword)}&lo=jp`;
+        setTimeout(function () {
+          if (!document.webkitHidden) {
+            window.location =
+              "https://apps.apple.com/jp/app/%E3%82%A6%E3%82%A3%E3%82%AD%E6%9C%80%E7%9F%AD%E5%88%B0%E9%81%94%E3%82%B2%E3%83%BC%E3%83%A0-%E3%82%A6%E3%82%A3%E3%82%AD%E7%9F%AD/id1563872394";
+          }
+        }, 25);
         document.location = launch_uri;
       }
       // android
