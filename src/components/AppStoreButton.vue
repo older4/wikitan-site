f9528e5099f09ae01c15f3008bef4ea7acb9662e
ryo <3955027+older4@users.noreply.github.com>
2021-04-23 14:37:03 +0900

    リファクタリング

---
+++ src/components/AppStoreButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-row>
+    <v-col cols="6">
+      <a
+        href="https://play.google.com/store/apps/details?id=com.yasaidev.wiki_jump_game"
+      >
+        <v-img
+          contain
+          :height="$vuetify.breakpoint.xsOnly ? 40 : 60"
+          :src="require('../assets/googleplay.png')"
+        ></v-img
+      ></a>
+    </v-col>
+    <v-col cols="6">
+      <a
+        href="https://apps.apple.com/jp/app/%E3%82%A6%E3%82%A3%E3%82%AD%E6%9C%80%E7%9F%AD%E5%88%B0%E9%81%94%E3%82%B2%E3%83%BC%E3%83%A0-%E3%82%A6%E3%82%A3%E3%82%AD%E7%9F%AD/id1563872394"
+      >
+        <v-img
+          contain
+          :height="$vuetify.breakpoint.xsOnly ? 40 : 60"
+          :src="require('../assets/appstore.png')"
+        ></v-img>
+      </a>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "AppStoreButton",
+};
+</script>
---
+++ src/components/ScreenShotCarousel.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-carousel hide-delimiters interval="4000" cycle>
+    <v-carousel-item
+      v-for="image in screenshots"
+      :key="image"
+      :src="image"
+      width="100%"
+      max-height="auto"
+      contain
+    ></v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+export default {
+  name: "ScreenShotCarousel",
+  data() {
+    return {
+      screenshots: [
+        "/img/screenshot/screenshot0.png",
+        "/img/screenshot/screenshot1.png",
+        "/img/screenshot/screenshot2.png",
+        "/img/screenshot/screenshot3.png",
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
